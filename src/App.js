5973ec8c1d33281fcef56e639faaca0fc52becf3
Jai Narayana <72011969+Jai14@users.noreply.github.com>
2024-10-26 06:22:18 +0530

    intialize

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Three from './MyThree';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Three />
+    </div>
+  );
+}
+
+export default App;
